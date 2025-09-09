7f55cdb3a405cbe8d61febe3c12546d9ce1ff4cf
zglar <stephanus.agung96@gmail.com>
2025-09-09 14:40:43 +0700

    Create Black Screen

--- main.py
+++ main.py
@@ -11,6 +11,15 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        screen.fill((0, 0, 0))
+
+        pygame.display.flip()
+
 
 if __name__ == "__main__":
     main()
