0d38cb8bcb32070905ad25f36db6cb19a777f0a9
zglar <stephanus.agung96@gmail.com>
2025-09-09 14:26:46 +0700

    Initial Commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import pygame
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+
+
+def main():
+    pygame.init()
+
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+
+if __name__ == "__main__":
+    main()
