7e1d5fe1bd3dc9aeea436a68b55b21f624250efe
zglar <stephanus.agung96@gmail.com>
2025-09-10 14:16:30 +0700

    Create Player

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from player import Player
 
 
 def main():
@@ -14,8 +15,11 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     #Set clock
-    clock = pygame.time.Clock
+    clock = pygame.time.Clock()
     dt = 0
+
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+
 
     #Initialize game screen
     while True:
@@ -25,10 +29,12 @@ def main():
         
         screen.fill((0, 0, 0))
 
+        player.draw(screen)
         pygame.display.flip()
 
     #Cap FPS
     dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
