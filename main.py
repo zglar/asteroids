4faeb95f93357217057b2c0064e6ffcca29abb6d
zglar <stephanus.agung96@gmail.com>
2025-09-10 13:31:32 +0700

    Add circleshape.py

--- main.py
+++ main.py
@@ -5,15 +5,19 @@ from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 def main():
     pygame.init()
 
+    #Starting text prompt
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    #Set screen size
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    #Set clock
     clock = pygame.time.Clock
     dt = 0
 
+    #Initialize game screen
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -23,6 +27,7 @@ def main():
 
         pygame.display.flip()
 
+    #Cap FPS
     dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
