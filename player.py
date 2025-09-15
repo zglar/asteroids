dc651fc9d4d2fd3fe7dcbc0656e1d2899bb25809
zglar <stephanus.agung96@gmail.com>
2025-09-15 14:25:29 +0700

    Add Shoot Timer

--- player.py
+++ player.py
@@ -1,12 +1,13 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED, PLAYER_SHOOT_COOLDOWN
 from shot import Shot
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shoot_timer = 0.0
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -41,12 +42,15 @@ class Player(CircleShape):
             self.rotate(+dt)
 
         if keys[pygame.K_w]:
-            self.move(-dt)
+            self.move(+dt)
         
         if keys[pygame.K_s]:
-            self.move(+dt)
+            self.move(-dt)
 
-        if keys[pygame.K_SPACE]:
+        if self.shoot_timer > 0:
+            self.shoot_timer -= dt
+
+        if keys[pygame.K_SPACE] and self.shoot_timer <= 0:
             self.shoot()
 
     def shoot(self):
@@ -54,3 +58,4 @@ class Player(CircleShape):
 
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         shot.velocity = forward * PLAYER_SHOOT_SPEED
+        self.shoot_timer = PLAYER_SHOOT_COOLDOWN
